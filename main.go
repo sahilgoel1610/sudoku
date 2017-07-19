6b0fac7c8a22a322219a7eae1b4152518d0a2a78
Sahil Goel <sahil.goel@olacabs.com>
2017-07-20 00:17:53 +0530

    Make 9x9 sodoku work

--- main.go
+++ main.go
@@ -9,44 +9,63 @@ type MatrixPointer struct {
 	Y int
 }
 
-func main() {
-	values := [4][4]int{
-		{1,2,3,0},
-		{0,3,4,1},
-		{3,4,1,2},
-		{4,1,0,3}}
 
+func main() {
+	values := [9][9]int{
+		{0,0,4,  0,9,6,  0,1,8},
+		{3,8,6,  0,0,4,  9,5,0},
+		{9,5,1,  0,3,0,  6,0,4},
+
+		{0,4,3,  0,0,0,  5,0,9},
+		{6,2,0,  0,0,0,  0,4,0},
+		{7,9,0,  4,0,2,  1,6,0},
+		
+		{0,0,7,	 6,2,1,	 0,9,5},
+		{0,0,0,  7,0,5,  0,3,0},
+		{0,6,2,  3,4,0,  7,8,1}}
+
+	// printMatrix(values)
+	resultMatrix := make(chan [9][9]int)
 	matrixPointer := findNextEmpty(values)
-	finalResult := canFillThisPlace(copyArray(values), matrixPointer.X, matrixPointer.Y)
+	finalResult := canFillThisPlace(copyArray(values), matrixPointer.X, matrixPointer.Y, resultMatrix)
 	fmt.Print("FINAL RESULT IF THE SODOKU IS SOLVABLE : ")
 	fmt.Println(finalResult)
-	time.Sleep(time.Second * 3)
+	commitMatrix(<- resultMatrix)
+	// fmt.Printf("final Matrix", <- resultMatrix)
+	time.Sleep(time.Second * 13)
 
 }
 
 
-func canFillThisPlace(matrix [4][4]int, i int, j int) bool {
-	resultChan := make(chan bool, 4)
-	for k := 1; k < 5; k++ {
+func canFillThisPlace(matrix [9][9]int, i int, j int, resultMatrix chan [9][9]int) bool {
+	resultChan := make(chan bool, 9)
+	for k := 1; k < 10; k++ {
 		a := copyArray(matrix)
-		go tryWithNumber(a, i, j, k, resultChan)
+		go tryWithNumber(a, i, j, k, resultChan, resultMatrix)
 	}
 	
-	return <- resultChan || <- resultChan || <- resultChan || <- resultChan
+	
+	for l := 0; l < 9; l++ {
+		if <- resultChan {
+			return true
+		}
+	}
+
+	return false
 
 }
 
 
-func tryWithNumber(a [4][4]int, i int, j int , k int, resultChan chan bool) {
+func tryWithNumber(a [9][9]int, i int, j int , k int, resultChan chan bool, resultMatrix chan [9][9]int) {
 	matrix := copyArray(a)
 	matrix[i][j] = k
 	if conditionsAtPositionValid(matrix, i, j) {
 		matrixPointer := findNextEmpty(matrix)
 		if matrixPointer.X == 0 && matrixPointer.Y == 0 {
 			resultChan <- true
-			commitMatrix(matrix)
+			resultMatrix <- matrix
 		} else {
-			resultChan <- canFillThisPlace(matrix, matrixPointer.X, matrixPointer.Y)
+			resultChan <- canFillThisPlace(matrix, matrixPointer.X, matrixPointer.Y, resultMatrix)
 		}
 	} else {
 		resultChan <- false
@@ -55,14 +74,14 @@ func tryWithNumber(a [4][4]int, i int, j int , k int, resultChan chan bool) {
 
 
 
-func conditionsAtPositionValid(matrix [4][4]int, i int, j int) bool {
-	for a := 0; a < 4; a++ {
+func conditionsAtPositionValid(matrix [9][9]int, i int, j int) bool {
+	for a := 0; a < 9; a++ {
 		if a != i && matrix[a][j] == matrix[i][j] {
 			return false;
 		}
 	}
 
-	for b := 0; b < 4; b++ {
+	for b := 0; b < 9; b++ {
 		if b != j && matrix[i][b] == matrix[i][j] {
 			return false;
 		}
@@ -72,9 +91,9 @@ func conditionsAtPositionValid(matrix [4][4]int, i int, j int) bool {
 }
 
 // Tested
-func findNextEmpty(matrix [4][4]int) MatrixPointer {
-	for i := 0; i < 4; i++ {
-		for j:= 0; j < 4; j++ {
+func findNextEmpty(matrix [9][9]int) MatrixPointer {
+	for i := 0; i < 9; i++ {
+		for j:= 0; j < 9; j++ {
 			if matrix[i][j] == 0 {
 				return MatrixPointer{i ,j}
 			}
@@ -85,10 +104,11 @@ func findNextEmpty(matrix [4][4]int) MatrixPointer {
 }
 
 
-func commitMatrix(matrix [4][4]int) {
-	for i := 0; i < 4; i++ {
+func commitMatrix(matrix [9][9]int) {
+	// time.Sleep(time.Second * 5)
+	for i := 0; i < 9; i++ {
 		fmt.Print("|")
-		for j:= 0; j < 4; j++ {
+		for j:= 0; j < 9; j++ {
 			fmt.Print(matrix[i][j])
 		}
 		fmt.Println("|")
@@ -96,12 +116,12 @@ func commitMatrix(matrix [4][4]int) {
 }
 
 
-func copyArray(matrix [4][4]int) [4][4]int {
+func copyArray(matrix [9][9]int) [9][9]int {
 
-	copiedArray := [4][4]int{}
+	copiedArray := [9][9]int{}
 	
-	for i := 0; i < 4; i++ {
-		for j:= 0; j < 4; j++ {
+	for i := 0; i < 9; i++ {
+		for j:= 0; j < 9; j++ {
 			copiedArray[i][j] = matrix[i][j]
 		}
 	}
